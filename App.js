c4d76c55d80d1e6e9ec3d5d518e0aa3eb82cd641
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-26 18:26:14 +0900

    #7.1 logIn and logOut

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Text, Image } from "react-native";
+import { Image } from "react-native";
+import { Provider } from "react-redux";
 import Gate from "./components/Gate";
+import store from "./redux/store";
 
 const cacheImages = images =>
   images.map(image => {
@@ -31,7 +33,9 @@ export default function App() {
     return Promise.all([...fontPromises, ...imagePromises]);
   };
   return isReady ? (
-    <Gate />
+    <Provider store={store}>
+      <Gate />
+    </Provider>
   ) : (
     <AppLoading
       onError={console.error}
--- components/Gate.js
+++ components/Gate.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { logIn, logOut } from "../redux/usersSlice";
 
 export default () => {
-  const isLoggedIn = false;
+  const { isLoggedIn } = useSelector(state => state.usersReducer);
+  const dispatch = useDispatch();
   return (
     <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      {isLoggedIn ? <Text>Welcome</Text> : <Text>Login please</Text>}
+      {isLoggedIn ? (
+        <TouchableOpacity onPress={() => dispatch(logOut())}>
+          <Text>Log Out</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={() => dispatch(logIn("bs.token"))}>
+          <Text>Log In</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
