448158fd59fde8ee9ee3f2c4375e70ebbee801ee
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-26 18:38:11 +0900

    #7.2 redux-persit part One

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
 import rootReducer from "./rootReducer";
+import { AsyncStorage } from "react-native";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 });
+
+const persistedStore = persistStore(store);
 
 export default store;
