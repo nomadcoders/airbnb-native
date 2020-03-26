5c1f2f466ab6c02f5ebed75622668d673c2d5e9d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-26 18:52:20 +0900

    #7.3 redux-persit part Two

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Image } from "react-native";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Gate from "./components/Gate";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 
 const cacheImages = images =>
   images.map(image => {
@@ -34,7 +35,9 @@ export default function App() {
   };
   return isReady ? (
     <Provider store={store}>
-      <Gate />
+      <PersistGate persistor={persistor}>
+        <Gate />
+      </PersistGate>
     </Provider>
   ) : (
     <AppLoading
--- redux/store.js
+++ redux/store.js
@@ -28,6 +28,6 @@ const store = configureStore({
   })
 });
 
-const persistedStore = persistStore(store);
+export const persistor = persistStore(store);
 
 export default store;
