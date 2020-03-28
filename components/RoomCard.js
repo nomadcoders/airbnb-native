bdd0db26d245650cec7fe51c3a1226b0cbeb2ca6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-28 18:01:31 +0900

    #9.6 RoomCard part Two

--- components/RoomCard.js
+++ components/RoomCard.js
@@ -2,7 +2,58 @@ import React from "react";
 import Pt from "prop-types";
 import styled from "styled-components/native";
 
-const RoomCard = ({ id, isFav, isSuperHost, photos, name, price }) => null;
+const Container = styled.View`
+  width: 100%;
+  margin-bottom: 50px;
+  align-items: flex-start;
+`;
+
+const Name = styled.Text`
+  font-size: 18px;
+  font-weight: 300;
+  margin-bottom: 7px;
+`;
+
+const Superhost = styled.View`
+  padding: 3px 5px;
+  border: 1px solid black;
+  border-radius: 4px;
+  margin-bottom: 5px;
+`;
+
+const SuperhostText = styled.Text`
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 10px;
+`;
+
+const PriceContainer = styled.View`
+  flex-direction: row;
+`;
+
+const PriceText = styled.Text`
+  font-size: 16px;
+`;
+
+const PriceNumber = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const RoomCard = ({ id, isFav, isSuperHost, photos, name, price }) => (
+  <Container>
+    {isSuperHost ? (
+      <Superhost>
+        <SuperhostText>Superhost</SuperhostText>
+      </Superhost>
+    ) : null}
+    <Name>{name}</Name>
+    <PriceContainer>
+      <PriceNumber>${price}</PriceNumber>
+      <PriceText> / night</PriceText>
+    </PriceContainer>
+  </Container>
+);
 
 RoomCard.propTypes = {
   id: Pt.number.isRequired,
--- screens/Main/Explore/ExplorePresenter.js
+++ screens/Main/Explore/ExplorePresenter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, ScrollView } from "react-native";
 import RoomCard from "../../../components/RoomCard";
 
 const Container = styled.View`
@@ -9,25 +9,28 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Text = styled.Text``;
-
 export default ({ rooms }) => {
   return (
     <Container>
       {rooms.length === 0 ? (
         <ActivityIndicator color="black" />
       ) : (
-        rooms.map(room => (
-          <RoomCard
-            key={room.id}
-            name={room.name}
-            price={room.price}
-            photos={room.photos}
-            id={room.id}
-            isFav={room.is_fav}
-            isSuperHost={room.user.superhost}
-          />
-        ))
+        <ScrollView
+          style={{ width: "100%", marginTop: 120 }}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+        >
+          {rooms.map(room => (
+            <RoomCard
+              key={room.id}
+              name={room.name}
+              price={room.price}
+              photos={room.photos}
+              id={room.id}
+              isFav={room.is_fav}
+              isSuperHost={room.user.superhost}
+            />
+          ))}
+        </ScrollView>
       )}
     </Container>
   );
